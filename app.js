da81e50031e5114bb6d1d5874faddea27b9384f3
giiki <guibollellalongo@gmail.com>
2022-01-17 08:11:56 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/assets", express.static(__dirname + "/assets"));
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.get("*", async function (req, res) {
+    res.render("../views/home", {});
+});
+
+const server = app.listen(process.env.PORT || 1370, () => {
+    const port = server.address().port;
+    console.log(`Express is working on port ${port}`);
+});
