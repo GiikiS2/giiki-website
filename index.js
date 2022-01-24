21b3b9a415390ec42d9ae71d9ef39fd167cb79a7
giiki <guibollellalongo@gmail.com>
2022-01-24 18:17:24 -0300

    lol

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({
 }))
 
 app.get("*", async function (req, res) {
-    res.render("../views/home", {});
+    res.render("./views/home", {});
 });
 
 const server = app.listen(process.env.PORT || 1370, () => {
